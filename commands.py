624f705debd451ea6f856cc2dbc351cbbd28a6d1
Yadav <jaikishan.yadav@jp-n3f254jlxy.intra.rakuten.co.jp>
2023-01-11 14:11:28 +0900

    Day 2 ssh update

--- commands.py
+++ commands.py
@@ -1,36 +1,84 @@
 # username password implementation
+'''
+Commands to interact with the terminal
+'''
+import os
+
+
+ATTEMPTS = 3
+ROOTPATH = '/Users/jaikishan.yadav/Documents/python-learning'
+HOSTPATH = '/Users/jaikishan.yadav/Documents/python-learning/hostxyz'
+SERVERNAME = 'hostxyz'
+userpswd = {"user1": "1234", 
+            "user2": "abcd", 
+            "user3": "u12312"}
+
+loginflags = {"user1": 0,
+              "user2": 0, 
+              "user3": 0}
+
+currentuser = "root"
+
 
 def ssh(cli_input):
-    import os
-    username, servername = cli_input.split('@')
-    attempts = 3
-    userpswd = {"user1": "7834", "user2": "abcd", "user3": "u12312"}
+    '''
+    SSH into a host
+
+    Args:
+        cli_input: a string in the form of [username@hostname]
+    '''
+    global currentuser
+    username, servername = cli_input.split('@') 
     flag = 0
-    if servername != "hostxyz":
+    if servername != SERVERNAME:
         print("Server doesn't exist")
         return
     for users, password in userpswd.items():
         if (username == users):
+            if loginflags[username] == 1:
+                print("User already logged in.")
+                if (currentuser != username):
+                    print("Switching User")
+                    os.chdir(HOSTPATH)
+                    currentuser = username
+                return
             flag = 1
-            while attempts:
+            tries = ATTEMPTS
+            while tries:
                 inp_passwords = input("Enter Password: ")
                 if (inp_passwords == password):
                     flag = 2
-                    os.chdir('./hostxyz')
+                    if (os.getcwd() != HOSTPATH):
+                        os.chdir(HOSTPATH)
+                    currentuser = username
+                    loginflags[username] = 1
                     print("Connection Established.")
                     print("You are currently at", os.getcwd())
                     break
                 else: 
-                    attempts -= 1
-                print("Wrong Password attempts left:", attempts)
+                    tries -= 1
+                print("Wrong Password attempts left:", tries)
     if flag == 1:
         print("Unable to establish connection.")
     if flag == 0:
         print("User not found")
 
+
 def logout():
-    import os
-    os.chdir('../')
+    '''
+    Logs a user out of the server. If not logged into a server, does nothing.
+    '''
+    global currentuser
+    if os.getcwd() != ROOTPATH:
+        loginflags[currentuser] = 0
+        currentuser = "root"
+        os.chdir('../')
     print("Logging out...")
     print("You are currently at ", os.getcwd())
 
+
+def whoami():
+    '''
+    Displays the current user
+    '''
+    print(currentuser)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3 -i
 
+import commands
+import os
 from commands import *
